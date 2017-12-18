8dbfaa0ec9be03eb8f478e442c9c3266a40befb7
Ruslan <ru_gr@ukr.net>
2017-12-18 17:35:39 +0200

    renamed views (sample - container)

--- src/main/java/browserJava/controllers/Controller.java
+++ src/main/java/browserJava/controllers/Controller.java
@@ -1,5 +1,11 @@
 package browserJava.controllers;
 
+import javafx.fxml.FXML;
+
 public class Controller {
 
+    @FXML
+    public void initialize(){
+        System.out.println("====== initialize Controller =======");
+    }
 }
