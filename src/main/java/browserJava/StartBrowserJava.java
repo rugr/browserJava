6118dda4eb3af1f0111afab95d3cafe6aa29e341
Ruslan <ru_gr@ukr.net>
2017-12-20 12:03:50 +0200

    icon program

--- src/main/java/browserJava/StartBrowserJava.java
+++ src/main/java/browserJava/StartBrowserJava.java
@@ -4,6 +4,7 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 /**
@@ -21,6 +22,7 @@ public class StartBrowserJava extends Application {
         Parent root = FXMLLoader.load(getClass().getResource("/views/container.fxml"));
         primaryStage.setTitle("RG BROWSER");
         primaryStage.setScene(new Scene(root, 1024, 768));
+        primaryStage.getIcons().add(new Image(getClass().getResource("/images/browser.png").toString()));
         primaryStage.show();
         System.out.println("===== start StartBrowserJava =======");
     }
